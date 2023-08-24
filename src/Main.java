6e2d6a44185d3ca26d9d67ae5cd8ad64d548aa07
TIBI <tiberiu.martin10@gmail.com>
2023-08-24 08:35:57 +0300

    new_branch

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
 
-        System.out.println(1+1);
+        int y = 2;
+        double x = 3 + 6 * y--;
+        System.out.println(x);
     }
 
 
