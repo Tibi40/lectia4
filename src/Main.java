e68ee45c641ea49863697dea51c9d59678ca745f
TIBI <tiberiu.martin10@gmail.com>
2023-08-24 09:02:49 +0300

    adding files

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        String name = "Mark";
+        String forName = "Tiberius";
+        name += forName;
+
+        System.out.println(name);
     }
 }
