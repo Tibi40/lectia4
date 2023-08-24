5150f4fcc1c2dd2d0059e01253f0c55d7d38aa4f
TIBI <tiberiu.martin10@gmail.com>
2023-08-24 13:02:14 +0300

    adding files

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,41 @@ public class Main {
         int y = 2;
         double x = 3 + 6 * y--;
         System.out.println(x);
+
+        if ((3 % 5) >= 1) {
+            System.out.println("yes");
+        } else {
+            System.out.println("No");
+        }
+
+        int q = 10;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(q++); // De ce se printeaza doar de 10 ori si nu de in infinit de ori
+
+            /* La q++ = numaratoare se face incepand cu valoarea initiala
+            la ++q = numaratoarea se face continuand cu valoarea initiala
+             */
+
+        boolean xw = true;
+        boolean yw = !xw;
+        int Dayhour = 10;
+
+        if(Dayhour > 8) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+        }
+
+        int counter = 0;
+
+        for(int i = 1; i < 5; i++) {
+            counter++;
+
+            if (counter == 3 ) {
+                System.out.println("Please change your password!");
+            }
+        }
     }
 
 
